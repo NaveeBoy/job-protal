0d2519a06c120bc529bb898dd47de65df1774219
Naveen Dulanjana <127006891+NaveeBoy@users.noreply.github.com>
2024-04-15 20:10:25 +0530

    form setup outlook

--- frontend/src/pages/admin/AdminAddPopUp.js
+++ frontend/src/pages/admin/AdminAddPopUp.js
@@ -1,44 +1,73 @@
-import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControlLabel, IconButton, Stack, TextField } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  FormControlLabel,
+  IconButton,
+  Stack,
+  TextField,
+} from "@mui/material";
 import FormControlContext from "@mui/material/FormControl/FormControlContext";
-import CloseIcon from "@mui/icons-material/Close"
+import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
 
 const AdminAddPopUp = () => {
-    const [open,openchange]=useState(false);
-    const functionopenpopup=()=>{
-        openchange(true);
-    }
-    const closepopup=()=>{
-        openchange(false);
-    }
-    return (
-        <div style={{textAlign:'center'}}>
-            
-            <Button onClick={functionopenpopup} color="success" variant="contained">+ Create Admin</Button>
-            <Dialog 
-            // fullScreen 
-            open={open} onClose={closepopup} fullWidth maxWidth="sm">
-                <DialogTitle>Create New Admin  <IconButton onClick={closepopup} style={{float:'right'}}><CloseIcon color="primary"></CloseIcon></IconButton>  </DialogTitle>
-                <DialogContent>
-                    {/* <DialogContentText>Do you want remove this user?</DialogContentText> */}
-                    <Stack spacing={2} margin={2}>
-                      <TextField variant="outlined" label="First Name"></TextField>
-                      <TextField variant="outlined" label="Last Name"></TextField>
-                      <TextField variant="outlined" label="E-Mail"></TextField>
-                      <TextField variant="outlined" label="Password"></TextField>
-                      <TextField variant="outlined" label="User Type : Admin" disabled='true' value="Admin" ></TextField>
-                      
-                      
-                      <Button color="primary" variant="contained">Create</Button>
-                    </Stack>
-                </DialogContent>
-                <DialogActions>
-                {/* <Button color="success" variant="contained">Yes</Button>
+  const [open, openchange] = useState(false);
+  const functionopenpopup = () => {
+    openchange(true);
+  };
+  const closepopup = () => {
+    openchange(false);
+  };
+  return (
+    <div style={{ textAlign: "center" }}>
+      <Button onClick={functionopenpopup} color="success" variant="contained">
+        + Create Admin
+      </Button>
+      <Dialog
+        // fullScreen
+        open={open}
+        onClose={closepopup}
+        fullWidth
+        maxWidth="sm"
+      >
+        <DialogTitle>
+          Create New Admin{" "}
+          <IconButton onClick={closepopup} style={{ float: "right" }}>
+            <CloseIcon color="primary"></CloseIcon>
+          </IconButton>{" "}
+        </DialogTitle>
+        <DialogContent>
+          {/* <DialogContentText>Do you want remove this user?</DialogContentText> */}
+          <Stack spacing={2} margin={2}>
+            <TextField variant="outlined" label="First Name"></TextField>
+            <TextField variant="outlined" label="Last Name"></TextField>
+            <TextField variant="outlined" label="E-Mail"></TextField>
+            <TextField variant="outlined" label="Password"></TextField>
+            <TextField variant="outlined" label="Confirm Password"></TextField>
+            <TextField
+              variant="outlined"
+              label="User Type : Admin"
+              disabled="true"
+              value="Admin"
+            ></TextField>
+
+            <Button color="primary" variant="contained">
+              Create
+            </Button>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          {/* <Button color="success" variant="contained">Yes</Button>
                     <Button onClick={closepopup} color="error" variant="contained">Close</Button> */}
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
-}
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
 
 export default AdminAddPopUp;
