bf39935b24afc1acee35d760ecdc8b3187190e59
Naveen Dulanjana <127006891+NaveeBoy@users.noreply.github.com>
2024-04-16 11:50:24 +0530

    User Reducer SignUp set

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -42,6 +42,29 @@ export const userReducerSignIn = (state = {}, action) => {
 
 }
 
+//user SignUp
+
+export const userReducerSignUp = (state = {}, action) => {
+    switch (action.type) {
+        case USER_SIGNIN_REQUEST:
+            return { loading: true, userInfo: null, isAuthenticated: false }
+        case USER_SIGNIN_SUCCESS:
+            return {
+
+                loading: false,
+                userInfo: action.payload,
+                isAuthenticated: true
+            }
+        case USER_SIGNIN_FAIL:
+            return { loading: false, userInfo: null, isAuthenticated: false, error: action.payload }
+        case USER_SIGNIN_RESET:
+            return {}
+        default:
+            return state;
+    }
+
+}
+
 //user profile
 export const userReducerProfile = (state = { user: null }, action) => {
     switch (action.type) {
